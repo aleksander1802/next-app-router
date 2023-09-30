4df8b891eabe7a0be6a4a52b7d875464328757e8
Alex2 <aleksander1802@gmail.com>
2023-09-30 17:58:09 +0300

    feat: added component p

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import { Button, Htag } from '@/components';
+import { Button, Htag, P } from '@/components';
 
 export default function Home() {
 	return (
 		<main>
 			<Htag tag="h1">Main page</Htag>
-			<Button appearance="primary" arrow='right'>Кнопка</Button>
+			<Button
+				appearance="primary"
+				arrow="right"
+			>
+				Кнопка
+			</Button>
+			<P size="m">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Consectetur, molestias aliquid. Rem excepturi minus, sequi
+				ratione saepe magni tenetur nulla!
+			</P>
 		</main>
 	);
 }
