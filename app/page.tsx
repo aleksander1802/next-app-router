45ab4d94cdc58092c112ca77bbc4987e9c11159d
Alex2 <aleksander1802@gmail.com>
2023-10-01 00:51:13 +0300

    feat: added rating component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import { Button, Htag, P, Tag } from '@/components';
+'use client';
+
+import { Button, Htag, P, Rating, Tag } from '@/components';
+import { useState } from 'react';
 
 export default function Home() {
+	const [rating, setRating] = useState(4);
+
 	return (
 		<main>
 			<Htag tag="h1">Main page</Htag>
@@ -15,12 +20,32 @@ export default function Home() {
 				Consectetur, molestias aliquid. Rem excepturi minus, sequi
 				ratione saepe magni tenetur nulla!
 			</P>
-			<Tag size='s'>Маленький</Tag>
-			<Tag size='m'>Средний</Tag>
-			<Tag size='m' color='red'>Red</Tag>
-			<Tag size='m' color='primary'>Primary</Tag>
-			<Tag size='m'>Ghost</Tag>
-			<Tag size='m' color='green'>Green</Tag>
+			<Tag size="s">Маленький</Tag>
+			<Tag size="m">Средний</Tag>
+			<Tag
+				size="m"
+				color="red"
+			>
+				Red
+			</Tag>
+			<Tag
+				size="m"
+				color="primary"
+			>
+				Primary
+			</Tag>
+			<Tag size="m">Ghost</Tag>
+			<Tag
+				size="m"
+				color="green"
+			>
+				Green
+			</Tag>
+			<Rating
+				rating={rating}
+				setRating={setRating}
+				isEditable
+			/>
 		</main>
 	);
 }
--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export * from '@/components/Htag/Htag';
 export * from '@/components/Button/Button';
 export * from '@/components/P/P';
 export * from '@/components/Tag/Tag';
+export * from '@/components/Rating/Rating';
