dbb91f51a8d6879fa177fff2bc919c7676c15e14
Alex2 <aleksander1802@gmail.com>
2023-10-07 15:17:44 +0300

    feat: added usescrolly hook

---
+++ hooks/useScrollY.ts
@@ -0,0 +1,21 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+
+export const useScrollY = (): number => {
+	const isBrowser = typeof window !== undefined;
+
+	const [scrollY, setScroll] = useState<number>(0);
+
+	const handleScroll = useCallback(() => {
+		const currentScrollY = isBrowser ? window.scrollY : 0;
+		setScroll(currentScrollY);
+	}, [isBrowser]);
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll, { passive: true });
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, [handleScroll]);
+
+	return scrollY;
+};
