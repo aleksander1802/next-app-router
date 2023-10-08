1a05a82e90224c5728f8be35745dd27438c1127c
Alex2 <aleksander1802@gmail.com>
2023-10-08 15:03:45 +0300

    fix: fixed metadata for type page

--- app/[type]/page.tsx
+++ app/[type]/page.tsx
@@ -1,27 +1,19 @@
-import { getPage } from '@/api/page';
 import { Htag } from '@/components';
 import { firstLevelMenu } from '@/helpers/helpers';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
-// export async function generateMetadata({
-// 	params,
-// }: {
-// 	params: { alias: string };
-// }): Promise<Metadata> {
-// 	const page = await getPage(params.alias);
 
-// 	return {
-// 		title: page?.metaTitle,
-// 	};
-// }
+export async function generateMetadata({
+	params,
+}: {
+	params: { type: string };
+}): Promise<Metadata> {
+	const char = params.type[0].toLocaleUpperCase() + params.type.slice(1);
 
-// export async function generateStaticParams() {
-// 	const menu = await getMenu(0);
-
-// 	return menu.flatMap((item) =>
-// 		item.pages.map((page) => ({ alias: page.alias })),
-// 	);
-// }
+	return {
+		title: `${char} page`,
+	};
+}
 
 export default async function PageAlias({
 	params,
