8194e3964a4033d1310242180464a2035586b733
Alex2 <aleksander1802@gmail.com>
2023-10-09 15:21:18 +0300

    feat: updated main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 import { Htag } from '@/components';
+import { pathMenu } from '@/helpers/path';
+import styles from './Mainpage.module.css';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<div>
-			<Htag tag="h1">Main page</Htag>
+		<div className={styles.mainPage}>
+			<Htag tag="h1">Главная страница</Htag>
+			<Htag tag="h2">Советую воспользоваться навигационным меню.</Htag>
+			<ul>
+				В данный момент доступно {pathMenu && pathMenu.length}{' '}
+				категории:
+				{pathMenu.map((item) => {
+					return (
+						<li key={item.route}>
+							<Htag tag="h2">
+								<span className={styles.link}>{item.name}</span>
+							</Htag>
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
--- helpers/path.ts
+++ helpers/path.ts
@@ -1,20 +1,24 @@
-import { TopLevelCategory } from "@/interfaces/page.interface";
+import { TopLevelCategory } from '@/interfaces/page.interface';
 
 export const pathMenu = [
 	{
 		route: 'courses',
+		name: 'Курсы',
 		id: TopLevelCategory.Courses,
 	},
 	{
 		route: 'services',
+		name: 'Сервисы',
 		id: TopLevelCategory.Services,
 	},
 	{
 		route: 'books',
+		name: 'Книги',
 		id: TopLevelCategory.Books,
 	},
 	{
 		route: 'products',
+		name: 'Продукты',
 		id: TopLevelCategory.Products,
 	},
 ];
